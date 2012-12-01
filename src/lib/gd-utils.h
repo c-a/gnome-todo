b58e6154d7de3f75eab7e6235d9f6f4a747ce3c6
Carl-Anton Ingmarsson <ca.ingmarsson@gmail.com>
2012-12-01 13:09:09 +0100

    Remove GdTaskListsIconView and GdTaskListRenderer.
    Also remove unused utility functions.

--- src/lib/gd-utils.h
+++ src/lib/gd-utils.h
@@ -26,23 +26,6 @@
 
 GtkCssProvider *gd_load_css_provider_from_resource (const char *path, GError **error);
 
-char *gd_filename_strip_extension (const char * filename_with_extension);
-
-gboolean gd_time_val_from_iso8601 (const gchar *string,
-                                   GTimeVal *timeval);
-gchar *gd_iso8601_from_timestamp (gint64 timestamp);
-
-GIcon *gd_create_collection_icon (gint base_size,
-                                  GList *pixbufs);
-GIcon *gd_create_symbolic_icon (const gchar *name,
-                                gint base_size);
-void   gd_entry_focus_hack (GtkWidget *entry,
-                            GdkDevice *device);
-
-GVariant *gd_create_variant_from_pixbuf (GdkPixbuf *pixbuf);
-
-gchar * gd_format_int_alternative_output (gint intval);
-
 GdkPixbuf* gd_draw_task_list (GPtrArray* items);
 
 #endif /* __GD_UTILS_H__ */
